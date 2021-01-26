d5a78c5d5a91c66cef31a727f2d905dbd1edab4c
MubbashirAhmedKhan <mubbashirahmedkhan876@gmail.com>
2021-01-26 09:26:45 +0500

    This is calculator app

---
+++ app.js
@@ -0,0 +1,30 @@
+var output = document.getElementById('output');
+buttons = document.querySelectorAll('button');
+var outputValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'x') {
+            buttonText = '*';
+            outputValue += buttonText;
+            output.value = outputValue;
+        }
+        else if (buttonText == 'C') {
+            outputValue = "";
+            output.value = outputValue;
+        }
+        else if (buttonText == '=') {
+            output.value = eval(outputValue);
+        }
+        else {
+            outputValue += buttonText;
+            output.value = outputValue;
+        }
+
+    })
+}
+
+
+
+
